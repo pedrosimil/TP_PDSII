4b36532bcb8bda89d850630370d5e569f49d8f14
pedrosimil <149528572+pedrosimil@users.noreply.github.com>
2024-07-05 18:37:54 -0300

    Update atividade.h

--- atividade.h
+++ atividade.h
@@ -1,1 +1,21 @@
+#ifndef ATIVIDADE_H
+#define ATIVIDADE_H
 
+#include <string>
+
+class Atividade {
+public:
+    Atividade(const std::string& titulo);
+    virtual ~Atividade();  // Destrutor virtual para liberação de memorias
+    virtual void exibirDetalhes() const = 0;  // Método virtual puro para forçar implementação nas classes derivadas
+
+   // virtual void excluir();
+    virtual void setTitulo(const std::string& novoTitulo);
+
+    std::string getTitulo() const;
+
+private:
+    std::string titulo;
+};
+
+#endif
