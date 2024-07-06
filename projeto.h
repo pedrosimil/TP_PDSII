945de7b7f159ea564ef4a60b7db296f5190f885a
pedrosimil <149528572+pedrosimil@users.noreply.github.com>
2024-07-05 22:27:45 -0300

    Update projeto.h

--- projeto.h
+++ projeto.h
@@ -1,1 +1,28 @@
+#include <iostream>    
+#include "atividade.h" 
+#include <vector>      
 
+
+class Projeto : public Atividade {
+public:
+    
+    Projeto(const std::string& titulo);
+
+   
+    virtual ~Projeto();
+
+    // Método para adicionar uma atividade ao projeto
+    void adicionarAtividade(Atividade* atividade);
+
+    // Método para exibir detalhes do projeto (override da função virtual na classe base)
+    void exibirDetalhes() const override;
+
+    // Método virtual para excluir o projeto
+    virtual void excluir();
+
+private:
+    // Vetor de ponteiros para Atividade para armazenar as atividades associadas ao projeto
+    std::vector<Atividade*> atividades;
+};
+
+#endif
